80f02cc33c0e92be5c4d4593c57368d0f012d4fc
YUIJ <2645602049@qq.com>
2022-12-20 14:09:27 +0800

    测试

--- index.py
+++ index.py
@@ -1,3 +1,11 @@
+"""
+Author: YUIJ 2645602049@qq.com
+Date: 2022-12-14 15:19:40
+LastEditors: YUIJ 2645602049@qq.com
+LastEditTime: 2022-12-18 16:50:32
+FilePath: \RandomCall\index.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+"""
 from bottle import Bottle, request, static_file, template
 import json
 from CONFIG import config
@@ -5,64 +13,87 @@ from CONFIG import config
 main = Bottle()
 
 
-class TemplateCenter:
-    @staticmethod
-    def page_main():
-        return template(config.子页位置.page_main())
-
-    @staticmethod
-    def randomcall():
-        return template(config.子页位置.randomcall())
-
-    @staticmethod
-    def randomcall_editor():
-        return template(config.子页位置.randomcall_editor())
-
+#############################################################################################
+# 网页
+#############################################################################################
 
 @main.route('/')
 def home():
-    return TemplateCenter.page_main()
+    return config.子页位置.page_main()
 
 
-@main.route('/page/main')
-def home():
-    return TemplateCenter.page_main()
-
-
-@main.route('/page/randomcall')
+@main.route('/randomcall')
 def index():
-    return TemplateCenter.randomcall()
+    return config.子页位置.randomcall()
 
 
-@main.route('/page/randomcall_editor')
+@main.route('/editor')
 def index():
-    return TemplateCenter.randomcall_editor()
+    return config.子页位置.randomcall_editor()
+
+
+@main.route('/login')
+def index():
+    return config.子页位置.randomcall_login()
+
+
+@main.route('/randomcall_test')
+def index():
+    return config.子页位置.randomcall_login()
+
+
+#############################################################################################
+# API
+#############################################################################################
 
 
 @main.route('/test')
-def test():
-    name = request.params.get('name')
-    data = {'name': name, 'age': 11223}
-    data = json.dumps(data)
+def index():
+    name = request.params.items()
+    name = dict(name)
+    # data = {'name': name, 'age': 11223,'error':'Null','model':'login'}
+    data = json.dumps(name)
+    print(data)
     return data
 
 
-# 根
-@main.route('/PROGRAMMER/css/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='./PROGRAMMER/css')
-
-
+#############################################################################################
 # 主题
-@main.route('/PROGRAMMER/css/theme/<filepath:path>')
+#############################################################################################
+
+
+@main.route('' + config.资源管理器.主题.根() + '<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='.' + config.资源管理器.主题.根())
+
+
+@main.route(config.资源管理器.主题.主题() + '/<filepath:path>')
 def server_static(filepath):
     return static_file(filepath, root='./PROGRAMMER/css/theme')
 
 
-# token
-@main.route('/PROGRAMMER/css/theme/Colormixer/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='./PROGRAMMER/css/theme/Colormixer')
+#############################################################################################
+# js
+#############################################################################################
+
+@main.route('/js/ajax')
+def index():
+    return config.资源管理器.js.ajax_helper()
+
+
+@main.route('/js/dev')
+def index():
+    return config.资源管理器.js.devtool()
+
+
+@main.route('/js/encryption')
+def index():
+    return config.资源管理器.js.encryption()
+
+
+@main.route('/js/config')
+def index():
+    return config.资源管理器.js.ajax_helper()
 
 
 main.run(host=config.host(), port=config.port(), reloader=True)
